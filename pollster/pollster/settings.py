291f36c8abf5c03d45139beedef26581da9da402
GershonD5 <42949021+GershonD5@users.noreply.github.com>
2020-03-08 21:25:09 -0400

    prehost

--- pollster/pollster/settings.py
+++ pollster/pollster/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a@3qcvdg$**f9)@iozc#onfd0zz9ejh$(n8civkcj^t1zl9u+m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
